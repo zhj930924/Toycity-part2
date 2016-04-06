02b66b9318721b8ffb484bd8662229736a4ff289
Jing <zhj930924@gmail.com>
2016-04-06 01:41:13 -0400

    Completed global setup
    create overall structure;
    define empty methods;
    create make bar method;
    create print date method.

--- lib/app.rb
+++ lib/app.rb
@@ -1,11 +1,58 @@
 require 'json'
-path = File.join(File.dirname(__FILE__), '../data/products.json')
-file = File.read(path)
-products_hash = JSON.parse(file)
+require 'date'
+
+def setup_files
+  path = File.join(File.dirname(__FILE__), '../data/products.json')
+  file = File.read(path)
+  $products_hash = JSON.parse(file)
+  # $report_file = File.new("report.txt", "w+")
+end
+
+def create_report
+  print_heading
+  print_data
+end
+
+def print_heading
+end
+
+def print_data
+  make_products_section
+  make_brands_section
+end
+
+def make_products_section
+
+end
+
+def make_brands_section
+
+end
+
+def start
+  setup_files # load, read, parse, and create the files
+  create_report # create the report!
+end
+
+# Calculate the max title length
+def find_max_title_length
+  setup_files
+  titles = $products_hash['items'].map { |toy| toy['title'] }
+  titles.max.length
+end
+
+# make ******************************** bar
+def make_bar
+  puts '*' * find_max_title_length
+end
 
 # Print "Sales Report" in ascii art
 
 # Print today's date
+def show_today_date
+  today = Date.today.strftime('%A, %B %d, %Y')
+  puts "Today's Date: #{today}"
+end
 
 # Print "Products" in ascii art
 
